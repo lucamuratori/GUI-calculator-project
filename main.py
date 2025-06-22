3dd739e85b2bff4cc86fcaed4a6dc74c12051a0d
Luca Muratori <lucamura399@gmail.com>
2025-06-22 04:56:15 +0200

    backend second commit

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def addition(*args: int):
+import math
+
+def addition(*args):
     x = sum(args)
     return x
 
-print(addition(1, 2, 3, 4, 5))
+
+def subtraction(*args):
+    args = list(args)
+    result = args.pop(0)
+    while args != []:
+        result -= args.pop(0)
+    return result
+
+
+def multiplication(*args):
+    
+    return
+print(subtraction(1, 2, 3, 4, 5))

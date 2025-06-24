de8262ae85fa835ae2a0db2169df3b3be60f3788
Luca Muratori <lucamura399@gmail.com>
2025-06-24 21:23:42 +0200

    added buttons to gui

--- gui.py
+++ gui.py
@@ -1,5 +1,37 @@
 import tkinter
+from tkinter import ttk
 
-
+# TODO: create the calculator gui
 root = tkinter.Tk()
+frame = ttk.Frame(root, padding=10)
+frame.grid()
+
+# buttons
+ttk.Button(frame, text="0").grid(column=1, row=6)
+ttk.Button(frame, text=".").grid(column=2, row=6)
+ttk.Button(frame, text="=").grid(column=4, row=6)
+ttk.Button(frame, text="1").grid(column=0, row=5)
+ttk.Button(frame, text="2").grid(column=1, row=5)
+ttk.Button(frame, text="3").grid(column=2, row=5)
+ttk.Button(frame, text="^").grid(column=3, row=5)
+ttk.Button(frame, text="+").grid(column=4, row=5)
+ttk.Button(frame, text="4").grid(column=0, row=4)
+ttk.Button(frame, text="5").grid(column=1, row=4)
+ttk.Button(frame, text="6").grid(column=2, row=4)
+ttk.Button(frame, text="n!").grid(column=3, row=4)
+ttk.Button(frame, text="-").grid(column=4, row=4)
+ttk.Button(frame, text="7").grid(column=0, row=3)
+ttk.Button(frame, text="8").grid(column=1, row=3)
+ttk.Button(frame, text="9").grid(column=2, row=3)
+ttk.Button(frame, text="√").grid(column=3, row=3)
+ttk.Button(frame, text="x").grid(column=4, row=3)
+ttk.Button(frame, text="AC").grid(column=0, row=2)
+ttk.Button(frame, text="⌫").grid(column=1, row=2)
+ttk.Button(frame, text="%").grid(column=2, row=2)
+ttk.Button(frame, text="root").grid(column=3, row=2)
+ttk.Button(frame, text="÷").grid(column=4, row=2)
+
+root.mainloop()
+# TODO: link all operations to the buttons on the calculator
+
 

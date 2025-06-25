3194e91378248e430793bc21de29db269d19d92a
Luca Muratori <lucamura399@gmail.com>
2025-06-25 21:24:23 +0200

    cleaned gui code

--- gui.py
+++ gui.py
@@ -10,21 +10,29 @@ frame.grid()
 ttk.Button(frame, text="0").grid(column=1, row=6)
 ttk.Button(frame, text=".").grid(column=2, row=6)
 ttk.Button(frame, text="=").grid(column=4, row=6)
+
+# row number 5
 ttk.Button(frame, text="1").grid(column=0, row=5)
 ttk.Button(frame, text="2").grid(column=1, row=5)
 ttk.Button(frame, text="3").grid(column=2, row=5)
 ttk.Button(frame, text="^").grid(column=3, row=5)
 ttk.Button(frame, text="+").grid(column=4, row=5)
+
+# row number 4
 ttk.Button(frame, text="4").grid(column=0, row=4)
 ttk.Button(frame, text="5").grid(column=1, row=4)
 ttk.Button(frame, text="6").grid(column=2, row=4)
 ttk.Button(frame, text="n!").grid(column=3, row=4)
 ttk.Button(frame, text="-").grid(column=4, row=4)
+
+# row number 3
 ttk.Button(frame, text="7").grid(column=0, row=3)
 ttk.Button(frame, text="8").grid(column=1, row=3)
 ttk.Button(frame, text="9").grid(column=2, row=3)
 ttk.Button(frame, text="√").grid(column=3, row=3)
 ttk.Button(frame, text="x").grid(column=4, row=3)
+
+# row number 2
 ttk.Button(frame, text="AC").grid(column=0, row=2)
 ttk.Button(frame, text="⌫").grid(column=1, row=2)
 ttk.Button(frame, text="%").grid(column=2, row=2)
